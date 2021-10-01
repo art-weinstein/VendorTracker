ecaa07bf8e2a28328b09ec069a3e3a05970ad551
Art Weinstein <artur.weinstein@gmail.com>
2021-10-01 09:46:46 -0700

    create passing test for title in order constructor

--- VendorTracker.Tests/ModelTests/OrderTests.cs
+++ VendorTracker.Tests/ModelTests/OrderTests.cs
@@ -6,13 +6,25 @@ using VendorTracker.Models;
 namespace VendorTracker.Tests
 {
   [TestClass]
-  public class OrderTests
+  public class OrderTests : IDisposable
   {
+    public void Dispose()
+    {
+      Order.ClearAll();
+    }
     [TestMethod]
     public void OrderConstructor_CreatesInstanceOfOrder_Order()
     {
       Order newOrder = new Order ("Title", "Description", "Price", "Date");
       Assert.AreEqual(typeof(Order), newOrder.GetType());
+    }
+    [TestMethod]
+    public void GetOrderTitle_ReturnOrderTitle_String()
+    {
+      string title = "Bread";
+      Order newOrder = new Order (title, "Description", "Price", "Date");
+      string result = newOrder.Title;
+      Assert.AreEqual(title, result);
     }
   }
 }
