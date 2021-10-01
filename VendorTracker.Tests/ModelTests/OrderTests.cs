16d5613afb874b77e45c81bce95433ccbbb5e61c
Art Weinstein <artur.weinstein@gmail.com>
2021-10-01 09:26:38 -0700

    create Order constructor and passing test for Order constructor.

---
+++ VendorTracker.Tests/ModelTests/OrderTests.cs
@@ -0,0 +1,18 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using VendorTracker.Models;
+
+namespace VendorTracker.Tests
+{
+  [TestClass]
+  public class OrderTests
+  {
+    [TestMethod]
+    public void OrderConstructor_CreatesInstanceOfOrder_Order()
+    {
+      Order newOrder = new Order ("Title", "Description", "Price", "Date");
+      Assert.AreEqual(typeof(Order), newOrder.GetType());
+    }
+  }
+}
